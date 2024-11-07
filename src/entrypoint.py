df54dcbc51106d7c34348d3bdc65366e84faaf21
Håvard K. Blixgård <havard.kvalvag.blixgard@hnikt.no>
2024-11-07 16:27:33 +0100

    merged

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -7,26 +7,26 @@ from teams_publisher import TeamsPublisher
 
 def prepare_event_data_and_call_notifier():
     # Retrieve the webhook URL from the environment variable
-    webhook_url = os.getenv("INPUT_WEBHOOK_URL")
+    webhook_url = os.getenv("WEBHOOK_URL")
+
+    # Full repo name
+    full_repo_name = os.getenv("GITHUB_REPOSITORY")
 
     # Load the pull request event data from the GitHub event payload file
     event_path = os.getenv("GITHUB_EVENT_PATH")
+    
     with open(event_path, 'r') as f:
         event_data = json.load(f)
 
     teams_publisher = TeamsPublisher(webhook_url)
-
     http_response_status_code = 0
-
     if "pull_request" in event_data.keys():
 
         # Prepare the payload to send to the webhook
         data = {
             "pr_number": event_data["number"],
-            "pr_title": event_data["pull_request"]["title"],
-            "pr_body": event_data["pull_request"]["body"],
-            "pr_user": event_data["pull_request"]["user"]["login"],
-            "pr_url": event_data["pull_request"]["html_url"]
+            "pr": event_data.get("pull_request", {}),
+            "repo": full_repo_name.split('/', 2)[1]
         }
         
         http_response_status_code = teams_publisher.send_pull_request_notification(data)
--- src/teams_publisher.py
+++ src/teams_publisher.py
@@ -1,5 +1,10 @@
 import os
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 import requests
+import json
+
+from jinja2 import Environment, FileSystemLoader
 
 
 class TeamsPublisher:
@@ -7,15 +12,41 @@ class TeamsPublisher:
         self.webhook_url = webhook_url
 
     def send_pull_request_notification(self, data):
-        return self.send_to_webhook(data)
+        """
+        Send a notification to the webhook URL about a new pull request.
+        """
+        template_path = template_path = os.path.join(os.path.dirname(__file__), "templates")
+        template_file = 'adaptive_card_template.json.j2'        
+        env = Environment(loader=FileSystemLoader(template_path))
+        template = env.get_template(template_file)
+ 
+        pr = data["pr"]
+        adaptive_card_message = template.render(
+            repo_name=data['repo'],
+            card_preview=f'Pull request - { data.get("repo") }',
+            body_title=f'Pull request - { data.get("repo") }',
+            body=pr['body'] or "No description provided.",
+            action_title='View Pull Request',
+            action_url=pr['html_url'],
+            user_login=pr['user']['login'],
+            user_url=pr['user']['html_url'],
+            user_avatar_url=pr['user']['avatar_url'],
+            created_at=datetime.strptime(pr['created_at'], '%Y-%m-%dT%H:%M:%SZ').astimezone(ZoneInfo("Europe/Oslo")).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            updated_at=datetime.strptime(pr['updated_at'], '%Y-%m-%dT%H:%M:%SZ').astimezone(ZoneInfo("Europe/Oslo")).strftime('%Y-%m-%dT%H:%M:%SZ')
+        )
+
+        try:
+            adaptive_card_message = json.loads(adaptive_card_message)
+        except Exception as e:
+            print(f"Failed to serialize pull request {pr.get('title')} in {data.get('repo')} with message: \n{adaptive_card_message} \nAnd exception: {e}")
+
+        # pr_data = {'text': 'Text for teams from Github Action'}
+        return self.send_to_webhook(adaptive_card_message)
     
     def send_to_webhook(self, payload):
-        # Set headers for the request
-        headers = {"Content-Type": "application/json"}
-
         # Send the request to the webhook
         try:
-            response = requests.post(self.webhook_url, json=payload, headers=headers)
+            response = requests.post(self.webhook_url, json=payload)
             response.raise_for_status()
             print(f"Response from webhook: {response.status_code}")
 
